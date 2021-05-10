0b8d3f2d9ca0f73fedb78943aa021f50f87b41dd
Chansik Yun <chansigi@gmail.com>
2021-05-10 15:59:12 +0900

    .

--- SampleApp2/SampleApp2/ViewController2.swift
+++ SampleApp2/SampleApp2/ViewController2.swift
@@ -6,26 +6,50 @@ import UIKit
 // => Interface 기반 이벤트 처리 방식
 //   1) dataSource: UIPickerViewDataSource
 //   2) delegate: UIPickerViewDelegate
-
-class ViewController2: UIViewController, UIPickerViewDataSource {
+class ViewController2:
+  UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
   @IBOutlet var pickerView: UIPickerView!
   @IBOutlet var nameLabel: UILabel!
 
+  let data = [
+    "Hello",
+    "World",
+    "Show",
+    "Me",
+    "The",
+    "Money"
+  ]
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // pickerView.dataSource = self
-    
+
+    pickerView.dataSource = self
+    pickerView.delegate = self
   }
-  
+
   // returns the number of 'columns' to display.
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
-    return 2
+    return 1
   }
 
   // returns the # of rows in each component..
   func pickerView(_ pickerView: UIPickerView,
-                  numberOfRowsInComponent component: Int) -> Int {
-    return 10
+                  numberOfRowsInComponent component: Int) -> Int
+  {
+    return data.count
+  }
+
+  func pickerView(_ pickerView: UIPickerView,
+                  titleForRow row: Int,
+                  forComponent component: Int) -> String?
+  {
+    return data[row]
+  }
+
+  func pickerView(_ pickerView: UIPickerView,
+                  rowHeightForComponent component: Int) -> CGFloat
+  {
+    return 48.0
   }
 }

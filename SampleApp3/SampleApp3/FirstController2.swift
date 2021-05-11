924541ad9ac0557c4318ef5b036f554a68117b14
Chansik Yun <chansigi@gmail.com>
2021-05-11 16:14:58 +0900

    .

---
+++ SampleApp3/SampleApp3/FirstController2.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+
+class FirstController2: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func onTapOpenButton(_ sender: UIButton) {
+  }
+  
+}

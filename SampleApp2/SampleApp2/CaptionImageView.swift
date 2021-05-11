1c869d7c919cb66bb17ffb475c4d98b58bedc5be
Chansik Yun <chansigi@gmail.com>
2021-05-11 14:41:54 +0900

    .

--- SampleApp2/SampleApp2/CaptionImageView.swift
+++ SampleApp2/SampleApp2/CaptionImageView.swift
@@ -10,24 +10,46 @@ import UIKit
 // IB / Storyboard 통해 생성할 경우
 // - init(coder: NSCoder)
 
-
+@IBDesignable
 class CaptionImageView: UIView {
   @IBOutlet var imageView: UIImageView!
-  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var nameLabel: UILabel! // Implicit Optional Type
+  //  - nil 체크 없이 사용할 수 있습니다.
+  //  - nil일 경우, 잘못된 참조로 인해 프로그램이 비정상 종료할 수 있습니다.
   @IBOutlet var contentView: UIView!
+
+  var caption: String? {
+    get {
+      return nameLabel?.text
+    }
+    set {
+      nameLabel?.text = newValue
+    }
+  }
+
+  var image: UIImage? {
+    get {
+      return imageView?.image
+    }
+    set {
+      imageView?.image = newValue
+    }
+  }
 
   override init(frame: CGRect) {
     super.init(frame: frame)
     initSubviews()
   }
-  
+
   required init?(coder: NSCoder) {
     super.init(coder: coder)
     initSubviews()
   }
-  
+
   func initSubviews() {
-    let nib = UINib(nibName: "CaptionImageView", bundle: nil)
+    // let nib = UINib(nibName: "CaptionImageView", bundle: nil)
+
+    let nib = UINib(nibName: "CaptionImageView", bundle: Bundle(for: type(of: self)))
     nib.instantiate(withOwner: self, options: nil)
 
     contentView.frame = bounds

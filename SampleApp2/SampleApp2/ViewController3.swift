7989c08a06dbf52108d17364279be0763f922e21
Chansik Yun <chansigi@gmail.com>
2021-05-11 14:43:36 +0900

    .

--- SampleApp2/SampleApp2/ViewController3.swift
+++ SampleApp2/SampleApp2/ViewController3.swift
@@ -2,9 +2,14 @@
 import UIKit
 
 class ViewController3: UIViewController {
+  
+  @IBOutlet var captionImageView: CaptionImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+    captionImageView.caption = "Kotlin"
+    captionImageView.image = #imageLiteral(resourceName: "kotlin")
   }
 }

12fa54a8f769c2e3beae310a9c083c5555c8941e
Chansik Yun <chansigi@gmail.com>
2021-05-12 17:05:04 +0900

    .

--- SampleApp4/SampleApp4/CollectionViewController.swift
+++ SampleApp4/SampleApp4/CollectionViewController.swift
@@ -6,24 +6,35 @@ class CollectionViewController: UIViewController {
   let data = [
     "kotlin",
     "swift",
-    "typesciprt",
+    "typescript",
     "kotlin",
     "swift",
-    "typesciprt",
+    "typescript",
     "kotlin",
     "swift",
-    "typesciprt",
+    "typescript",
     "kotlin",
     "swift",
-    "typesciprt",
+    "typescript",
   ]
-  
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
     collectionView.dataSource = self
+    collectionView.delegate = self
+  }
+}
+
+// 구현되면, 호출됩니다.
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt _: IndexPath) -> CGSize
+  {
+    let width = (collectionView.frame.size.width - 20) / 2
+    return CGSize(width: width, height: width)
   }
 }
 
@@ -31,20 +42,20 @@ extension CollectionViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return data.count
   }
-  
+
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell",
                                                   for: indexPath) as! MyColCell
-    
+
     cell.updateUI(name: data[indexPath.row])
     return cell
   }
 
   /*
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
-    cell.backgroundColor = UIColor.yellow
-    return cell
-  }
-  */
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+     cell.backgroundColor = UIColor.yellow
+     return cell
+   }
+   */
 }

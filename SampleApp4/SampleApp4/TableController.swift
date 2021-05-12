18e3610907eac71f61afc5acf10cc5bfe4408fa8
Chansik Yun <chansigi@gmail.com>
2021-05-12 13:44:22 +0900

    .

---
+++ SampleApp4/SampleApp4/TableController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class TableController: UIViewController {
+  
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var nameLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func onTap(_ sender: UIButton) {
+  }
+}
--- SampleApp4/SampleApp4/ViewController.swift
+++ SampleApp4/SampleApp4/ViewController.swift
@@ -16,11 +16,14 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-  
+
   @IBAction func open(_ sender: UIButton) {
 //    let rootController = FirstController()
 //    let naviController = UINavigationController(rootViewController: rootController)
-//    
+//
 //    present(naviController, animated: true)
+
+    let controller = TableController()
+    present(controller, animated: true)
   }
 }

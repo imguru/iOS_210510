cd00a0b58532b63c1cb22d96c3532ce26fdc5b07
Chansik Yun <chansigi@gmail.com>
2021-05-12 16:49:17 +0900

    .

--- SampleApp4/SampleApp4/MyColCell.swift
+++ SampleApp4/SampleApp4/MyColCell.swift
@@ -1,4 +1,12 @@
 
 import UIKit
 
-class MyColCell: UICollectionViewCell {}
+class MyColCell: UICollectionViewCell {
+  @IBOutlet var coverImageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+
+  func updateUI(name: String) {
+    nameLabel.text = name
+    coverImageView.image = UIImage(named: name)
+  }
+}
